eccdc4663a6c61e36c9ede9fa7dabee0ad0f782b
Mersi Siagian <mersisuryanisiagian32148@gmail.com>
2023-06-12 15:03:25 +0700

    add test case

---
+++ Object Repository/Contact/Page_Contact - Matakail Communication/send message_button.rs
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>send message_button</name>
+   <tag></tag>
+   <elementGuidId>7b2233fc-fb8e-4a50-846b-985eab6c65a5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>button.btn-curve.btn-blc > span</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//form[@id='contact-form']/div[2]/button/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>57e184b4-603a-4578-8a3c-497bf80f7cb2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Send Message</value>
+      <webElementGuid>30bae248-16ae-45b1-be0a-851af81a9a65</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;contact-form&quot;)/div[@class=&quot;controls&quot;]/button[@class=&quot;btn-curve btn-blc&quot;]/span[1]</value>
+      <webElementGuid>a0f8f568-4907-4079-a374-6ed8962831e2</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='contact-form']/div[2]/button/span</value>
+      <webElementGuid>5d441a74-9232-4b19-b8f7-3ca676084d33</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='The project is awesome.'])[1]/following::span[1]</value>
+      <webElementGuid>d4b54f0f-9225-41ae-870e-9e0fbf280496</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Get In Touch.'])[1]/following::span[1]</value>
+      <webElementGuid>16027c43-7117-46e2-832a-a2321eba214f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Contact Info.'])[1]/preceding::span[1]</value>
+      <webElementGuid>3e579afd-b8b6-451a-8e17-9f817e59a542</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)=concat('Let', &quot;'&quot;, 's Talk')])[1]/preceding::span[1]</value>
+      <webElementGuid>136bad29-5760-43db-8b7c-c7af0ac78f0d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Send Message']/parent::*</value>
+      <webElementGuid>fac92ab1-8870-4b10-be78-0f16575a8b87</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/button/span</value>
+      <webElementGuid>c68a63e7-e18e-4936-8587-b9d478426170</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//span[(text() = 'Send Message' or . = 'Send Message')]</value>
+      <webElementGuid>b59791a3-f759-4fdb-87a0-0b19ce6d0474</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
